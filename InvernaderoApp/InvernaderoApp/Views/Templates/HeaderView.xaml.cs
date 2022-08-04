a5b879383edcc43d0098dcfd773d2b40b0f3d84b
Eric Domenzain <ericdomenzain@gmail.com>
2022-08-04 11:53:01 -0500

    actualización de imágenes

--- InvernaderoApp/InvernaderoApp/Views/Templates/HeaderView.xaml.cs
+++ InvernaderoApp/InvernaderoApp/Views/Templates/HeaderView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using InvernaderoApp.Dtos;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,13 @@ namespace InvernaderoApp.Views.Templates
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HeaderView : ContentView
     {
+        private Usuario usuario;
         public HeaderView()
         {
             InitializeComponent();
+            this.usuario = JsonConvert.DeserializeObject<Usuario>(Application.Current.Properties["usuario"].ToString());
+
+            lblNombre.Text = this.usuario.nombre.ToUpper();
         }
     }
 }
